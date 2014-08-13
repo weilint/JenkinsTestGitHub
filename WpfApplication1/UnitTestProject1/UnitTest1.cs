428ddaaf76762234650299ca61c056d9c717304c
weilint <weilint@array.co.jp>
2014-08-13 16:57:43 +0900

    add unit test

---
+++ WpfApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            WpfApplication1.MainWindow appl = new WpfApplication1.MainWindow();
+            Microsoft.VisualStudio.TestTools.UnitTesting.PrivateObject pobject = new PrivateObject(appl);
+            Button btn = (Button)pobject.GetFieldOrProperty("btn1");
+            Assert.IsNotNull(btn);
+        }
+    }
+}
